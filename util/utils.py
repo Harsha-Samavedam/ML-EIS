7fcbb624c137d9c5f4b0badfa411fcdb59559f97
Akhil Samavedam <akhil.samavedam@gmail.com>
2023-01-21 00:42:12 -0600

    Moved old data with inconsistent naming into data/old

--- util/utils.py
+++ util/utils.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from scipy import stats
+import math
 
 def real_imaginary(impedance : np.array, phase : np.array):
     return impedance * np.cos(np.radians(phase)), impedance * np.sin(np.radians(phase))
@@ -10,10 +11,15 @@ def average_data(filename : str, len : int):
     dss = [np.array(df[:]) for df in dfs]
     sum = np.zeros(shape=dss[0].shape)
     for ds in dss: sum += ds
-    return pd.DataFrame(sum / len, columns=['Frequency', 'Impedance', 'Phase', 'Real', 'Imaginary', 'Magnitude'])
+    return pd.DataFrame(sum / len, columns=['Frequency', 'Impedance', 'Phase', 'Real', 'Imaginary', 'Magnitude', ' '])
 
-def drop_outliers(df : pd.DataFrame):
-    return df[(np.abs(stats.zscore(df[2])) < 3)]
+def drop_outliers(dx : pd.DataFrame):
+    df = dx[dx['Phase'] > -95]
+    return df[(np.abs(stats.zscore(df['Impedance'] * np.sin(np.sin(np.radians(df['Phase']))))) < 1.5)]
+
+
+def prepare_data(filename : str, len : int):
+    drop_outliers(average_data(filename, len)).to_csv(f'{filename}.csv')
 
 
 

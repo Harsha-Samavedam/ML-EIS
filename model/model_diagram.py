d84bb3d9116e9c2bf3d61b58e88879e8995b2c07
Akhil Samavedam <akhil.samavedam@gmail.com>
2023-02-05 20:52:57 -0600

    Added Subscripts for Impedance and Phase Terms in Model Diagrams and consistent File Structures through Directory Rooting

--- model/model_diagram.py
+++ model/model_diagram.py
@@ -1,14 +1,20 @@
 from sklearn.tree import export_graphviz
 import pickle as pkl
 import os
+from def_model import PKG_DIR
+
+os.chdir(PKG_DIR)
 
 model = pkl.load(open('MODEL', 'rb'))
 
 os.chdir('dot')
 
+SUB = str.maketrans("0123456789", "₀₁₂₃₄₅₆₇₈₉")
+SUP = str.maketrans("0123456789", "⁰¹²³⁴⁵⁶⁷⁸⁹")
+
 for i in range(len(model.estimators_)):
     export_graphviz(model.estimators_[i], out_file=f'tree{i + 1}.dot',
-        feature_names=['Φ' + str(i + 1) for i in range(512)] + ['z' + str(i + 1) for i in range(512)],
+        feature_names=['Φ' + str(i + 1).translate(SUB) for i in range(512)] + ['|Z|' + str(i + 1).translate(SUB) for i in range(512)],
         class_names=['Polystyrene', 'Nitrate', 'Sunscreen', 'Plain'],
         rounded=True, filled=True)
 

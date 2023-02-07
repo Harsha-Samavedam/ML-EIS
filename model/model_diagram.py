77ef6976f01cd87f145c6ebb688913ff3bcc7824
Akhil Samavedam <akhil.samavedam@gmail.com>
2023-02-07 01:30:26 -0600

    Fixed Phase and Impedance Numbering Issue in Decision Tree Diagrams

--- model/model_diagram.py
+++ model/model_diagram.py
@@ -14,7 +14,7 @@ SUP = str.maketrans("0123456789", "⁰¹²³⁴⁵⁶⁷⁸⁹")
 
 for i in range(len(model.estimators_)):
     export_graphviz(model.estimators_[i], out_file=f'tree{i + 1}.dot',
-        feature_names=['Φ' + str(i + 1).translate(SUB) for i in range(512)] + ['|Z|' + str(i + 1).translate(SUB) for i in range(512)],
+        feature_names=['Φ' + str((i + 1) // 2).translate(SUB) if (i + 1) % 2 == 0 else '|Z|' + str(((i + 1) // 2) + 1).translate(SUB) for i in range(1024)],
         class_names=['Polystyrene', 'Nitrate', 'Sunscreen', 'Plain'],
         rounded=True, filled=True)
 
